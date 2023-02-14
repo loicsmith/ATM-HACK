7e06ff82486ca61b49ae7ac9b8f7cd78a078ab9c
Loic <Loic@CHAMBRE>
2023-02-14 14:49:56 +0100

    Ajoutez des fichiers projet.

---
+++ RPPlusConfig.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace RPPlus
+{
+    [Serializable]
+    public class RPPlusConfig
+    {
+        public long hackCooldown;
+        public int minMoney;
+        public int maxMoney;
+        public int hackDuration;
+
+        public void Save()
+        {
+            string contents = JsonConvert.SerializeObject((object)this);
+            File.WriteAllText(RoleplayPlus.ConfPath, contents);
+        }
+    }
+}
